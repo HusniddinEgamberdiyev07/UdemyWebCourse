cf76bea81e5cffafec545bfc7509587e3054f7b6
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2026-07-05 13:07:06 +0500

    this in event

---
+++ events/btns.js
@@ -0,0 +1,22 @@
+for(let i =0; i<=10; i++){
+    let btn = document.createElement("button");
+    btn.append("Click me");
+    btn.classList.add("btn");
+    document.body.append(btn)
+
+    btn.addEventListener("click", changeColor)
+}
+
+function randColor(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+function changeColor(){
+    let color = randColor();
+    this.style.backgroundColor = color;
+    this.innerText = color
+}
