07d2bf0afebdbe34a8f4f4b33a20aa6e465d8c9d
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2026-07-06 11:26:31 +0500

    scorekeeper mini project

---
+++ scorekeeper/app.js
@@ -0,0 +1,83 @@
+const limit = document.querySelector("#limit");
+const scoreLabel = document.querySelector("#scoreLabel");
+const playerOneBtn = document.querySelector("#playerOneBtn");
+const playerTwoBtn = document.querySelector("#playerTwoBtn");
+const reset = document.querySelector("#reset");
+const playerOneLabel = document.querySelector("#playerOne");
+const playerTwoLabel = document.querySelector("#playerTwo");
+const scoreKeeper = document.querySelector(".scoreKeeper");
+
+let maxScore = 1;
+let playerOneCount = 0;
+let playerTwoCount = 0;
+
+const winnerDiv = document.createElement("div");
+winnerDiv.classList.add("winnerDiv");
+
+const winnerLabel = document.createElement("h2")
+winnerDiv.append(winnerLabel)
+
+
+function changeLabel(){
+    playerOneLabel.innerText = playerOneCount;
+    playerTwoLabel.innerText = playerTwoCount;
+}
+
+function stopGame(){
+    playerOneBtn.disabled = true;
+    playerTwoBtn.disabled = true;
+}
+
+function chooseWinner(){
+    if (playerOneCount === maxScore) {
+        stopGame()
+        playerOneLabel.classList.add("winner");
+        playerTwoLabel.classList.add("loser");
+
+        winnerLabel.innerText = "Player One WON"
+        scoreKeeper.append(winnerDiv)
+
+    } else if(playerTwoCount === maxScore){
+        stopGame()
+        playerTwoLabel.classList.add("winner");
+        playerOneLabel.classList.add("loser");
+
+        winnerLabel.innerText = "Player Two WON"
+        scoreKeeper.append(winnerDiv)
+    }
+}
+
+limit.addEventListener("change", function(){
+    maxScore = parseInt(this.value);
+})
+
+playerOneBtn.addEventListener("click", ()=>{
+    playerOneCount+=1;
+    chooseWinner();
+    changeLabel();
+});
+
+playerTwoBtn.addEventListener("click", ()=>{
+    playerTwoCount+=1;
+    chooseWinner();
+    changeLabel();
+});
+
+reset.addEventListener("click", ()=>{
+    playerOneCount = 0;
+    playerTwoCount = 0;
+    playerOneBtn.disabled = false;
+    playerTwoBtn.disabled = false;
+    changeLabel();
+
+    if(playerOneLabel.classList.contains("winner")) {
+        playerOneLabel.classList.remove("winner")
+        playerTwoLabel.classList.remove("loser")
+    }
+    else {
+        playerOneLabel.classList.remove("loser")
+        playerTwoLabel.classList.remove("winner")
+    }
+    
+    scoreKeeper.removeChild(winnerDiv);
+})
