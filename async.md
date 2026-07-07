89702f063997ded9436d55f4f3d9c82fe4370c83
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2026-07-07 11:21:43 +0500

    promise resolved rejected pending

--- async.md
+++ async.md
@@ -3,3 +3,11 @@
 Interpreter to keep track of its place in a script that calls multiple functions — what function is currently being run and what functions are called from within that function, etc.
 
 **Js is single threaded**
+
+**Promises**
+
+It is an object. It represents failure or success of an asynronous operation.
+It can be pending, resolved and rejected.
+
+.then(callback) -> resolved
+.catch(callback) -> rejected
--- async/app.js
+++ async/app.js
@@ -63,3 +63,57 @@ delayChangeColor("red", 1500, ()=>{
         delayChangeColor("green", 1500);
     })
 });
+
+
+// Promises
+
+const fakeRequestPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 4000) {
+                reject('Connection Timeout :(')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
+}
+
+// fakeRequestPromise("somehwere.com")
+//     .then(()=>{
+//         console.log("Success 1");
+
+//         fakeRequestPromise("somewhere.com/page1")
+//             .then(()=>{
+//                 console.log("success 2");
+//             })
+//             .catch(()=>{
+//                 console.log("Failure 2");
+//             })
+//     })
+//     .catch(()=>{
+//         console.log("failure 1");
+//     })
+
+
+fakeRequestPromise("somehwere.com")
+    .then((data)=>{
+        console.log(data);
+        console.log("Success 1");
+
+        return fakeRequestPromise("somewhere.com/page1")
+    })
+    .then((data)=>{
+        console.log(data);
+        console.log("Success page1");
+        
+        return fakeRequestPromise("somewhere.com/page2")
+    })
+    .then((data)=>{
+        console.log(data);
+        console.log("Success page3");
+    })
+    .catch(()=>{
+        console.log("Rejected");
+    })
