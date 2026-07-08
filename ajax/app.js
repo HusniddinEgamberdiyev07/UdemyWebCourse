33cc1e456569345bec677a084cba7726aedcd0f4
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2026-07-08 13:48:37 +0500

    better format

--- ajax/app.js
+++ ajax/app.js
@@ -4,28 +4,20 @@
 
 const req = new XMLHttpRequest();
 
-req.onload = function(){
-    console.log("Loaded");
-    const data = JSON.parse(this.responseText)
-    // console.log(data.result.properties.name);
-    // console.log(data.result.properties.height);
-}
+req.onload = function () {
+  console.log("Loaded");
+  const data = JSON.parse(this.responseText);
+  // console.log(data.result.properties.name);
+  // console.log(data.result.properties.height);
+};
 
-req.onerror = function(){
-    console.log("Error");
-    console.log("this");
-}
+req.onerror = function () {
+  console.log("Error");
+  console.log("this");
+};
 
 req.open("GET", "https://swapi.tech/api/people/1");
-req.send()
-
-
-
-
-
-
-
-
+req.send();
 
 // newest fetch api
 
@@ -34,57 +26,49 @@ req.send()
 // res.json() returns a promise
 
 fetch("https://swapi.tech/api/people/1")
-    .then(res=>{
-        console.log("First request resolved.");
-        console.log(res);
-        return res.json();
-    })
-    .then(data=>{
-        console.log("Data: ", data);
-    })
-    .catch(err=>console.log("Something went wrong."))
-
-
-
-
-
-
-
-
-
+  .then((res) => {
+    console.log("First request resolved.");
+    console.log(res);
+    return res.json();
+  })
+  .then((data) => {
+    console.log("Data: ", data);
+  })
+  .catch((err) => console.log("Something went wrong."));
 
 // chain request
 
 fetch("https://swapi.tech/api/people/1")
-    .then(res=>{
-        // console.log("First request resolved.");
-        // console.log(res);
-        return res.json();
-    })
-    .then(data=>{
-        // console.log("Data: ", data);
-        return fetch("https://swapi.tech/api/people/2")
-    })
-    .then(res=>{
-        // console.log("Sdecond request resolved.");
-        // console.log(res);
-        return res.json();
-    })
-    .then(data=>{
-        // console.log("Data: ", data);
-    })
-    .catch(err=>console.log("Something went wrong."))
-
-
-
+  .then((res) => {
+    // console.log("First request resolved.");
+    // console.log(res);
+    return res.json();
+  })
+  .then((data) => {
+    // console.log("Data: ", data);
+    return fetch("https://swapi.tech/api/people/2");
+  })
+  .then((res) => {
+    // console.log("Sdecond request resolved.");
+    // console.log(res);
+    return res.json();
+  })
+  .then((data) => {
+    // console.log("Data: ", data);
+  })
+  .catch((err) => console.log("Something went wrong."));
 
 // best version with async&aiwat
 
-async function getPeople(){
-    const res = await fetch("https://swapi.tech/api/people/1")
+async function getPeople() {
+  try {
+    const res = await fetch("https://swapi.tech/api/people/1");
     const data = await res.json();
     console.log(data);
-    const res2 = await fetch("https://swapi.tech/api/people/2")
+    const res2 = await fetch("https://swapi.tech/api/people/2");
     const data2 = await res2.json();
     console.log(data2);
+  } catch (e) {
+    console.log(e);
+  }
 }
